bd943580d286b5755fdf863b97bf68d4b7640756
Dyjuen <denardyjuen@gmail.com>
2024-11-20 12:28:55 +0700

    init

--- main.cpp
+++ main.cpp
@@ -77,6 +77,35 @@ void exitProgram() {
     exit(0);
 }
 
+void transaction() {
+    std::unordered_map<std::string, std::function<void()>> menu = {
+    {"1", viewSummary},
+    {"2", addTransaction},
+    {"3", editTransaction},
+    {"4", deleteTransaction},
+    };
+
+    std::string choice;
+    while (true) {
+        std::cout << "\n-------- Transactions --------\n";
+        std::cout << "1. View Summary\n";
+        std::cout << "2. Add Transaction\n";
+        std::cout << "3. Edit Transaction\n";
+        std::cout << "4. Delete Transaction\n";
+        std::cout << "5. Back to Main Menu\n";
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+        auto it = menu.find(choice);
+        if (it != menu.end()) {
+            it->second();
+        } 
+		else {
+            std::cout << "Returning to Main Menu\n";
+            return;
+        }
+    }
+}
+
 
 void reports() {
     std::unordered_map<std::string, std::function<void()>> menu = {
@@ -107,35 +136,6 @@ void reports() {
     }
 }
 
-
-void settings() {
-    std::unordered_map<std::string, std::function<void()>> menu = {
-    {"1", manageCategories},
-    {"2", setBudgetLimits},
-    {"3", notificationPreferences},
-    {"4", securitySettings},
-    };
-
-    std::string choice;
-    while (true) {
-        std::cout << "\n-------- Settings --------\n";
-        std::cout << "1. Manage Categories\n";
-        std::cout << "2. Set Budget Limits\n";
-        std::cout << "3. Notification Preferences\n";
-        std::cout << "4. Security Settings\n";
-        std::cout << "5. Back to Main Menu\n";
-        std::cout << "Enter your choice: ";
-        std::cin >> choice;
-        auto it = menu.find(choice);
-        if (it != menu.end()) {
-            it->second();
-        } 
-		else {
-            std::cout << "Returning to Main Menu\n";
-            return;
-        }
-    }
-}
 
 void advancedFeatures() {
     std::unordered_map<std::string, std::function<void()>> menu = {
@@ -200,36 +200,58 @@ void help() {
     }
 }
 
-int main() {
+void settings() {
     std::unordered_map<std::string, std::function<void()>> menu = {
-        {"1", viewSummary},
-        {"2", addTransaction},
-        {"3", editTransaction},
-        {"4", deleteTransaction},
-        {"5", reports},
-        {"6", settings},
-        {"7", advancedFeatures},
-        {"8", help},
-        {"9", exitProgram}
+    {"1", manageCategories},
+    {"2", setBudgetLimits},
+    {"3", notificationPreferences},
+    {"4", securitySettings},
+    {"5", advancedFeatures},
+    {"6", help},
     };
 
     std::string choice;
     while (true) {
-        // Display the menu
+        std::cout << "\n-------- Settings --------\n";
+        std::cout << "1. Manage Categories\n";
+        std::cout << "2. Set Budget Limits\n";
+        std::cout << "3. Notification Preferences\n";
+        std::cout << "4. Security Settings\n";
+        std::cout << "5. Advanced Features\n";
+        std::cout << "6. Help\n";
+        std::cout << "7. Back to Main Menu\n";
+        std::cout << "Enter your choice: ";
+        std::cin >> choice;
+        auto it = menu.find(choice);
+        if (it != menu.end()) {
+            it->second();
+        } 
+		else {
+            std::cout << "Returning to Main Menu\n";
+            return;
+        }
+    }
+}
+
+
+int main() {
+    std::unordered_map<std::string, std::function<void()>> menu = {
+        {"1", transaction},
+        {"2", reports},
+        {"3", settings},
+        {"4", exitProgram}
+    };
+
+    std::string choice;
+    while (true) {
         std::cout << "\n-------- Main Menu --------\n";
-        std::cout << "1. View Summary\n";
-        std::cout << "2. Add Transaction\n";
-        std::cout << "3. Edit Transaction\n";
-        std::cout << "4. Delete Transaction\n";
-        std::cout << "5. Reports\n";
-        std::cout << "6. Settings\n";
-        std::cout << "7. Advanced Features\n";
-        std::cout << "8. Help\n";
-        std::cout << "9. Exit\n";
+        std::cout << "1. Transactions\n";
+        std::cout << "2. Reports\n";
+        std::cout << "3. Settings\n";
+        std::cout << "4. Exit Program\n";
         std::cout << "Enter your choice: ";
         std::cin >> choice;
 
-        // Execute the corresponding function
         auto it = menu.find(choice);
         if (it != menu.end()) {
             it->second();
